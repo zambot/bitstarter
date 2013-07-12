e77b658c3a6d1c651786d2a8c8a4cc26ad59b3e4
Jan Steinmetz <jan@kopfmaschine.com>
2013-07-13 00:56:57 +0200

    make use of readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  string = fs.readFileSync("index.html");
+  response.send(string.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
